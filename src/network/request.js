4cf8415a3eca8a9879891bff49dc5d1db65127bf
ShenjxAdmin <1833319622@qq.com>
2020-03-12 11:23:31 +0800

    '开发提交'

---
+++ src/network/request.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+export function request(config){
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/api/wh',
+    // baseURL: "http://106.54.54.237:8000/api/wh" // 备用地址
+    timeout: 5000
+  })
+  
+  // 请求拦截器
+  instance.interceptors.request.use(config => {
+      console.log('请求成功、执行的拦截器操作')
+      return config
+    },
+  err => {
+    console.log(err)
+  })
+  
+  // 响应拦截器
+  instance.interceptors.response.use(resp => {
+      console.log('响应成功、执行的拦截器操作')
+    return resp.data
+  },
+  err => {
+    console.log(err)
+  })
+  
+  // 发送请求
+  return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <h2>首页</h2>
+  <nav-bar class="home-nav">
+    <div slot="center">购物街</div>
+  </nav-bar>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar"
+  import {getHomeMultiData} from "api/home"
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data(){
+      return{
+        banners: [],
+        recommends: []
+      }
+    },
+    created(){
+      getHomeMultiData().then(res => {
+        console.log(res)
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+    box-shadow: 0px -3px 1px rgba(100, 100, 100, 0.1); /* 设置光圈 */
+  }
 </style>
